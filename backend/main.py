ad7994b97e4030b5b0839e036b012f317aff432f
tuanhungtran678 <tuanhungtran678@gmail.com>
2026-06-16 14:22:37 +0700

    #

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
+from email.mime import audio
 from fastapi import FastAPI, UploadFile, File
+from basic_pitch import ICASSP_2022_MODEL_PATH, predict_and_save
 import os
 
 app = FastAPI()
 
 os.makedirs("uploads", exist_ok=True)
+os.makedirs("generated/midi", exist_ok=True)
 
 @app.get("/")
 def root():
@@ -16,14 +19,39 @@ async def upload_audio(
     audio: UploadFile = File(...)
 ):
 
-    filepath = f"uploads/{audio.filename}"
+    if not audio.filename.endswith((".mp3", ".wav")):
+    return {
+        "success": False,
+        "message": "Only MP3 and WAV files allowed"
+    }
 
+    filepath = f"uploads/{audio.filename}"
+    predict_and_save(
+    audio_path_list=[filepath],
+    output_directory="generated/midi",
+    save_midi=True,
+    sonify_midi=False,
+    save_model_outputs=False,
+    save_notes=False,
+    model_or_model_path=ICASSP_2022_MODEL_PATH
+)
+    
     with open(filepath, "wb") as buffer:
         buffer.write(
             await audio.read()
         )
-
+    midi_filename = audio.filename.replace(".mp3", "_basic_pitch.mid")
     return {
-        "success": True,
-        "filename": audio.filename
+    "success": True,
+    "audio_file": audio.filename,
+    "midi_file": midi_filename
     }
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
--- frontend/js/upload.js
+++ frontend/js/upload.js
@@ -1,18 +1,41 @@
-const btn =
-document.getElementById("uploadBtn");
+async function uploadAudio() {
+    const fileInput = document.getElementById("audioFile");
 
-btn.addEventListener("click", () => {
-
-    const file =
-    document.getElementById("audioFile")
-    .files[0];
-
-    if(!file){
+    if (!fileInput.files.length) {
         alert("Chọn file trước!");
         return;
     }
 
-    document.getElementById("status")
-    .textContent =
-    `Đã chọn: ${file.name}`;
-});
+    const file = fileInput.files[0];
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    document.getElementById("status").innerText =
+        "Đang upload...";
+
+    try {
+
+        const response = await fetch(
+            "https://ai-detected-notes-and-change-to-sheet.onrender.com/upload",
+            {
+                method: "POST",
+                body: formData
+            }
+        );
+
+        const data = await response.json();
+
+        console.log(data);
+
+        document.getElementById("status").innerText =
+            "Hoàn thành!";
+
+    } catch (error) {
+
+        console.error(error);
+
+        document.getElementById("status").innerText =
+            "Lỗi!";
+    }
+}
